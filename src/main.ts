01793ed03982154294efbeba0925c508e1c044eb
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-11 14:19:09 +0530

    check-deployment

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { prisma } from './prisma';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   await prisma.$connect();
   await app.listen(process.env.PORT ?? 3000);
