4fc3e433483953238d11edd0257b4a855b182ae0
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-12 17:53:13 +0530

    check

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
   app.enableCors();
   app.use(cookieParser());
 
