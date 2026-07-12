c862848742231331f4989968f39266050f9ea4c7
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-12 18:05:10 +0530

    maybe???

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,8 @@ async function bootstrap() {
   });
   app.enableCors();
   app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
