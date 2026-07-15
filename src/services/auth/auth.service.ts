ce30db52abc9e170fe85328532f3e048eb57455a
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-15 20:51:23 +0530

    refresh_toekn_addiction

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -58,6 +58,8 @@ export class AuthService {
     const oAuthProvider: string = body['oAuthProvider'] as string;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const accessToken: string = body['accessToken'] as string;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const refreshToken: string = body['accessToken'] as string;
     try {
       const user = await this.prisma.uSER.findUnique({
         where: { email: email },
@@ -75,7 +77,7 @@ export class AuthService {
       }
 
       const token = this.JWT.sign(
-        { email: email, accessToken: accessToken },
+        { email: email, accessToken: accessToken, refreshToken: refreshToken },
         { expiresIn: '30m', secret: process.env.JWT_SECRET },
       );
 
