025c2c7511448e1b824007f83b886062be7f4692
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-18 17:10:19 +0530

    done-check-now

---
+++ prisma/migrations/20260718114005_done_auth/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `expiresIn` on the `USER` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "USER" DROP COLUMN "expiresIn";
