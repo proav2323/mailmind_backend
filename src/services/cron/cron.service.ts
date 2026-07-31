f4073cc6451ee04f0dedb2bd887348f09b7da90c
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-07-31 23:46:09 +0530

    check-if-cron-works-at-1-am

--- src/services/cron/cron.service.ts
+++ src/services/cron/cron.service.ts
@@ -1,17 +1,32 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { EmailsService } from '../emails/emails.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { generateId } from 'src/utils/generateId';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
-  constructor(private emailsService: EmailsService) {}
+  constructor(
+    private emailsService: EmailsService,
+    private prismaService: PrismaService,
+  ) {}
 
-  @Cron('0 2 * * *', {
+  @Cron('0 1 * * *', {
     timeZone: 'Asia/Kolkata',
   })
   async handleCron() {
     this.logger.log('Running task at 2:00 AM!');
+    await this.prismaService.uSER.create({
+      data: {
+        email: 'testing@gmail.com',
+        id: generateId(8),
+        name: 'test smith',
+        oAuthProvider: 'google',
+        photoUrl: 'hdhd',
+        refreshToken: '',
+      },
+    });
     await this.emailsService.changePriorities();
     this.logger.log('task completed at 2:00 AM!');
   }
