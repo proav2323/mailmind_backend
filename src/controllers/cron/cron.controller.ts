e9e42a053dd344e48d2088989082b536040b17bd
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-03 12:29:22 +0530

    now-new-route-handeler-to-sync-prioities

--- src/controllers/cron/cron.controller.ts
+++ src/controllers/cron/cron.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Headers } from '@nestjs/common';
+import { Controller, Headers, UnauthorizedException } from '@nestjs/common';
 import { Get } from '@nestjs/common';
 import { CronService } from '../../services/cron/cron.service';
+import { EmailsService } from 'src/services/emails/emails.service';
 
 @Controller('cron')
 export class CronController {
-  constructor(private cronService: CronService) {}
+  constructor(
+    private cronService: CronService,
+    private emailsService: EmailsService,
+  ) {}
   @Get('priorities')
   async syncPriorities(@Headers('authorization') authHeader: string) {
     await this.cronService.handleCron(authHeader);
+    return { message: 'Priorities synced successfully' };
+  }
+
+  @Get('sync')
+  async syncPrioritiesRealtime(@Headers('authorization') authHeader: string) {
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      throw new UnauthorizedException('Invalid cron secret');
+    }
+    await this.emailsService.changePriorities();
     return { message: 'Priorities synced successfully' };
   }
 }
--- src/services/cron/cron.service.ts
+++ src/services/cron/cron.service.ts
@@ -2,12 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { EmailsService } from '../emails/emails.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { generateId } from 'src/utils/generateId';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class CronService {
   constructor(
     private emailsService: EmailsService,
     private prismaService: PrismaService,
+    private httpService: HttpService,
   ) {}
 
   async handleCron(authHeader: string) {
@@ -16,7 +18,9 @@ export class CronService {
       throw new UnauthorizedException('Invalid cron secret');
     }
     console.log('running cron job to change email priorities...');
-    await this.emailsService.changePriorities();
+    this.httpService.get('https://mailmind-backend.onrender.com/cron/sync', {
+      headers: { authorization: `Bearer ${process.env.CRON_SECRET}` },
+    });
     await this.prismaService.uSER.create({
       data: {
         email: 'testing@gmail.com',
