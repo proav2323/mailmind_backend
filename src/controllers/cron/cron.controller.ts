37c811876b48e4a432bbe7937f5c5d54f01364a9
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-03 11:47:31 +0530

    cron-at-120:00pm to check

--- src/controllers/cron/cron.controller.ts
+++ src/controllers/cron/cron.controller.ts
@@ -1,11 +1,11 @@
 import { Controller } from '@nestjs/common';
-import { Post } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 import { CronService } from '../../services/cron/cron.service';
 
 @Controller('cron')
 export class CronController {
   constructor(private cronService: CronService) {}
-  @Post('priorities')
+  @Get('priorities')
   async syncPriorities() {
     await this.cronService.handleCron();
     console.log(`done`);
