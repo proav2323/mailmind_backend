dc54f45352135bf0c9fdbe344a7f882318dae366
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-09 13:49:11 +0530

    now?

--- src/services/pub-sub/pub-sub.service.ts
+++ src/services/pub-sub/pub-sub.service.ts
@@ -1,14 +1,4 @@
-import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
-import { PubSub } from '@google-cloud/pubsub';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PubSubService implements OnModuleInit {
-  private pubSubClient!: PubSub;
-  private readonly logger = new Logger(PubSubService.name);
-
-  onModuleInit() {
-    this.pubSubClient = new PubSub({
-      projectId: process.env.GCP_PROJECT_ID,
-    });
-  }
-}
+export class PubSubService {}
