148044cd45611892cd0d4cda93835e6e243f52ad
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-09 17:25:18 +0530

    compression_added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import * as express from 'express';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,6 +16,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  // app.use(compression.default());
 
   await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
 }
