2986a46edb25257a7973a7073c757f3ae528f164
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-12 14:37:28 +0530

    unNeccersry_token_fix

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -359,11 +359,10 @@ export class AuthService {
 
     const fid = user.fids.find((value) => value!['fid'] === FID);
 
-    if (fid!['token'] === TOKEN) {
-      return 'done';
-    }
-
     if (fid) {
+      if (fid['token'] === TOKEN) {
+        return 'done';
+      }
       const newFids = user.fids.filter((value) => value!['fid'] !== FID);
       newFids.push({ fid: FID, token: TOKEN, platform: PLATFROM });
       await this.prisma.uSER.update({
