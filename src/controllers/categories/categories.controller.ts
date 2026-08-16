32686974ee67782ad92a7b6c55b5ce9c54a48297
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-16 15:41:06 +0530

    add_delete_give_id_category

--- src/controllers/categories/categories.controller.ts
+++ src/controllers/categories/categories.controller.ts
@@ -1,14 +1,42 @@
-import { Controller, Get, Headers, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { CategoriesService } from '../../services/categories/categories.service';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private categoryService: CategoriesService) {}
+
   @Get('user')
   async getUserCatgeoires(
     @Req() req: Request,
     @Headers() headers: Record<string, string>,
+    @Body() { name }: { name: string },
+  ) {
+    return await this.categoryService.addCategory(req, headers, name);
+  }
+
+  @Post('add')
+  async addCategory(
+    @Req() req: Request,
+    @Headers() headers: Record<string, string>,
   ) {
     return await this.categoryService.getUserCategories(req, headers);
+  }
+
+  @Delete('delete/:id')
+  async deleteCategory(
+    @Req() req: Request,
+    @Headers() headers: Record<string, string>,
+    @Param('id') id: string,
+  ) {
+    return await this.categoryService.deleteCategory(req, headers, id);
   }
 }
--- src/services/categories/categories.service.ts
+++ src/services/categories/categories.service.ts
@@ -1,6 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
+import { generateId } from '../../utils/generateId';
 
 @Injectable()
 export class CategoriesService {
@@ -41,7 +47,104 @@ export class CategoriesService {
 
     return await this.prisma.cATEGORIES.findMany({
       where: { userId: user.id },
-      select: { name: true },
+      select: { name: true, id: true },
+    });
+  }
+
+  async addCategory(
+    req: Request,
+    headers: Record<string, string>,
+    name: string,
+  ) {
+    const token = (req as Request & { cookies?: Record<string, string> })
+      .cookies?.token;
+    let secondToken: string | undefined = undefined;
+    if (headers.authorization !== null && headers.authorization !== undefined) {
+      secondToken = headers.authorization.split(' ')[1];
+    }
+
+    if (!token && !secondToken) {
+      console.log('no token');
+      throw new BadRequestException('token not valid');
+    }
+
+    const decoded = this.JWT.verify<{
+      email: string;
+      scopes: string[];
+      scope: string;
+    }>(token !== undefined && token !== null ? token : secondToken!, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    const user = await this.prisma.uSER.findUnique({
+      where: { email: decoded.email },
+      select: { id: true },
+    });
+
+    if (!user) {
+      throw new BadRequestException('user not found');
+    }
+
+    return await this.prisma.cATEGORIES.create({
+      data: {
+        userId: user.id,
+        id: generateId(8),
+        name: name,
+      },
+      select: { name: true, id: true },
+    });
+  }
+
+  async deleteCategory(
+    req: Request,
+    headers: Record<string, string>,
+    id: string,
+  ) {
+    const token = (req as Request & { cookies?: Record<string, string> })
+      .cookies?.token;
+    let secondToken: string | undefined = undefined;
+    if (headers.authorization !== null && headers.authorization !== undefined) {
+      secondToken = headers.authorization.split(' ')[1];
+    }
+
+    if (!token && !secondToken) {
+      console.log('no token');
+      throw new BadRequestException('token not valid');
+    }
+
+    const decoded = this.JWT.verify<{
+      email: string;
+      scopes: string[];
+      scope: string;
+    }>(token !== undefined && token !== null ? token : secondToken!, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    const user = await this.prisma.uSER.findUnique({
+      where: { email: decoded.email },
+      select: { id: true },
+    });
+
+    if (!user) {
+      throw new BadRequestException('user not found');
+    }
+
+    const category = await this.prisma.cATEGORIES.findUnique({
+      where: { id: id },
+      select: { userId: true },
+    });
+
+    if (!category) {
+      throw new NotFoundException('category nto found');
+    }
+
+    if (category.userId !== user.id) {
+      throw new UnauthorizedException('unathotized attempt to delete category');
+    }
+
+    return await this.prisma.cATEGORIES.delete({
+      where: { id: id },
+      select: { name: true, id: true },
     });
   }
 }
